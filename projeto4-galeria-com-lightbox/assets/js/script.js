700b49057cddb93edd7265b162e3b6c8154dc71d
Bruno Alejandro <brunoalejandrodev@gmail.com>
2024-05-07 20:41:16 -0300

    js: add fechar modal ao clicar fora da imagem

--- projeto4-galeria-com-lightbox/assets/js/script.js
+++ projeto4-galeria-com-lightbox/assets/js/script.js
@@ -14,15 +14,26 @@ galleryItems.forEach((item)=>{
         lightboxImage.setAttribute('src', imageUrl);
 
         // colocando o display flex na lightbox para retirar o display none toda vez que uma foto for selecionada
-        lightbox.style.display = 'flex';
+        if (lightbox.classList.contains('fecharModal')) {
+            lightbox.classList.replace('fecharModal' ,'abrirModal');
+            document.body.style.overflow = 'hidden';
+        }
     })
 });
 
 lightbox.addEventListener('click', (event) => {
     //console.log(event.target);
-    lightbox.style.display = 'none';
+    if (lightbox.classList.contains('abrirModal') && event.target === lightbox) {
+        lightbox.classList.remove('abrirModal');
+        lightbox.classList.add('fecharModal');
+        document.body.style.overflow = 'auto';
+    }
 })
 
 btnLightboxClose.addEventListener("click", () => {
-    lightbox.style.display = 'none';
+    if (lightbox.classList.contains('abrirModal')) {
+        lightbox.classList.remove('abrirModal');
+        lightbox.classList.add('fecharModal');
+        document.body.style.overflow = 'auto';
+    }
 });
